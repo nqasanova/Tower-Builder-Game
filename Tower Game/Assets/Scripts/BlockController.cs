2452fb30e030cfd91a68a85cf848b4e7bfd9c004
Ibrahim Aliyev <109238723+ibrhm01@users.noreply.github.com>
2025-04-01 00:25:28 +0400

    Seeting up the project and adding BlockController Script

---
+++ Tower Game/Assets/Scripts/BlockController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Rigidbody rb;
+
+    private bool hasLanded = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!hasLanded && collision.gameObject.CompareTag("Ground"))
+        {
+            hasLanded = true;
+            GameManager.Instance.AddScore(10); // Give points
+        }
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - GetMouseWorldPos();
+        rb.isKinematic = true; // Disable physics while dragging
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+        rb.isKinematic = false; // Re-enable physics
+    }
+
+    void Update()
+{
+    if (transform.position.y < -5f) // Block fell off
+    {
+        GameManager.Instance.GameOver();
+    }
+}
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
